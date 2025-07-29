2be61ae93bbc9e32d7dbfc5ede78209f20d46735
Lionel <lionelpinkhard@me.com>
2025-07-29 10:24:31 +0200

    Update documentation

--- Reference/search/functions_e.js
+++ Reference/search/functions_e.js
@@ -30,12 +30,13 @@ var searchData=
   ['resetallseencounters_27',['resetAllSeenCounters',['../dd/d06/class_u_articy_expresso_scripts.html#ae12b89706b81baa5ea7e63f9d8f22c6a',1,'UArticyExpressoScripts']]],
   ['resetpackages_28',['ResetPackages',['../d5/d1d/struct_f_articy_package_defs.html#a96159e376c4ae7f400a3b01b89077143',1,'FArticyPackageDefs']]],
   ['resetvisited_29',['ResetVisited',['../db/d18/class_u_articy_global_variables.html#a65613ede2f2cb768cf64abf13b23050c',1,'UArticyGlobalVariables']]],
-  ['resolvecachedversion_30',['ResolveCachedVersion',['../d3/da9/class_u_articy_import_data.html#afcd09c0568814580aabd335a7b57f1c0',1,'UArticyImportData']]],
-  ['resolvesupportedclass_31',['ResolveSupportedClass',['../d0/dd9/class_u_articy_j_s_o_n_factory.html#a5c6ceb671df202c8a66d96daacad2bf4',1,'UArticyJSONFactory']]],
-  ['restorecachedfiles_32',['RestoreCachedFiles',['../d8/d15/class_code_generator.html#af4b28a2afb31402d8dd31ee85c015618',1,'CodeGenerator']]],
-  ['retrievearticyid_33',['RetrieveArticyId',['../d7/d2a/class_f_articy_id_customization.html#abc39aea538da5594e75a97f086532d0d',1,'FArticyIdCustomization']]],
-  ['retrievearticyref_34',['RetrieveArticyRef',['../db/db8/class_f_articy_ref_customization.html#a80f60d7b924d170a4d371f5e5551efbf',1,'FArticyRefCustomization']]],
-  ['retrieveclass_35',['RetrieveClass',['../d2/d17/namespace_articy_importer_helpers.html#ac7537d20bbea08196f8ef1d1df5c0f3c',1,'ArticyImporterHelpers']]],
-  ['retrievepreviewimage_36',['RetrievePreviewImage',['../dc/d11/namespace_user_interface_helper_functions.html#a8f1bd181da7323b7591882620e1baa09',1,'UserInterfaceHelperFunctions']]],
-  ['retrievespeakerpreviewimage_37',['RetrieveSpeakerPreviewImage',['../dc/d11/namespace_user_interface_helper_functions.html#a1d5d203e280eb9b930838499d7535ddd',1,'UserInterfaceHelperFunctions']]]
+  ['resolvearticydirectory_30',['ResolveArticyDirectory',['../d1/d7f/class_u_articy_import_commandlet.html#a5b3627fd107402fe4660e6faed06d79c',1,'UArticyImportCommandlet']]],
+  ['resolvecachedversion_31',['ResolveCachedVersion',['../d3/da9/class_u_articy_import_data.html#afcd09c0568814580aabd335a7b57f1c0',1,'UArticyImportData']]],
+  ['resolvesupportedclass_32',['ResolveSupportedClass',['../d0/dd9/class_u_articy_j_s_o_n_factory.html#a5c6ceb671df202c8a66d96daacad2bf4',1,'UArticyJSONFactory']]],
+  ['restorecachedfiles_33',['RestoreCachedFiles',['../d8/d15/class_code_generator.html#af4b28a2afb31402d8dd31ee85c015618',1,'CodeGenerator']]],
+  ['retrievearticyid_34',['RetrieveArticyId',['../d7/d2a/class_f_articy_id_customization.html#abc39aea538da5594e75a97f086532d0d',1,'FArticyIdCustomization']]],
+  ['retrievearticyref_35',['RetrieveArticyRef',['../db/db8/class_f_articy_ref_customization.html#a80f60d7b924d170a4d371f5e5551efbf',1,'FArticyRefCustomization']]],
+  ['retrieveclass_36',['RetrieveClass',['../d2/d17/namespace_articy_importer_helpers.html#ac7537d20bbea08196f8ef1d1df5c0f3c',1,'ArticyImporterHelpers']]],
+  ['retrievepreviewimage_37',['RetrievePreviewImage',['../dc/d11/namespace_user_interface_helper_functions.html#a8f1bd181da7323b7591882620e1baa09',1,'UserInterfaceHelperFunctions']]],
+  ['retrievespeakerpreviewimage_38',['RetrieveSpeakerPreviewImage',['../dc/d11/namespace_user_interface_helper_functions.html#a1d5d203e280eb9b930838499d7535ddd',1,'UserInterfaceHelperFunctions']]]
 ];
