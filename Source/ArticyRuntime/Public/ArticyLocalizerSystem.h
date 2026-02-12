b5411558f120135400e2742bc33a72b2c834d24b
Lionel <lionelpinkhard@me.com>
2026-02-12 16:43:09 +0200

    Fix define log class

--- Source/ArticyRuntime/Public/ArticyLocalizerSystem.h
+++ Source/ArticyRuntime/Public/ArticyLocalizerSystem.h
@@ -14,6 +14,8 @@
 #include "ArticyLocalizationSubsystem.h"
 #include "ArticyLocalizerSystem.generated.h"
 
+DEFINE_LOG_CATEGORY(LogArticyRuntime)
+
 UCLASS(BlueprintType)
 class ARTICYRUNTIME_API UArticyLocalizerSystem : public UObject
 {
